85caff75de0bf8e9c2bcf8710317c9ac16b97a7e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-10-21 08:49:49 +0200

    Update grpc-api, grpc-core, ... to 1.33.0 (#200)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val scodecBits       = "1.1.20"
     val circe            = "0.13.0"
     val scalaPb          = scalapb.compiler.Version.scalapbVersion
-    val grpc             = "1.32.2"
+    val grpc             = "1.33.0"
     val disciplineSpecs2 = "1.1.0"
     val specs2           = "4.10.5"
     val catsEffectSpecs2 = "0.4.1"
