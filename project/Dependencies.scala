b51b555f21e49f3792bd0924b0f09744ea31a6ac
Scala Steward <me@scala-steward.org>
2021-04-02 12:03:18 +0200

    Update log4cats-core, log4cats-noop, ... to 2.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val catsCore         = "2.5.0"
     val catsEffect       = "2.4.1"
     val fs2              = "2.5.4"
-    val log4cats         = "1.2.2"
+    val log4cats         = "2.0.1"
     val logback          = "1.2.3"
     val scodecBits       = "1.1.25"
     val circe            = "0.13.0"
