bf2bb30f61347acb749ac67e35c98f10f44f8e14
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-06-25 09:35:04 +0200

    Update specs2-cats, specs2-core, ... to 4.12.2 (#398)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val scalaPb          = scalapb.compiler.Version.scalapbVersion
     val grpc             = "1.38.1"
     val disciplineSpecs2 = "1.1.6"
-    val specs2           = "4.12.1"
+    val specs2           = "4.12.2"
     val catsEffectSpecs2 = "1.1.1"
     val tsConfig         = "1.4.1"
 
