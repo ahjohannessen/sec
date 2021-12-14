31fdb5b04f2abb2a9df08afa3288ff3f28196f56
Scala Steward <me@scala-steward.org>
2021-12-14 20:04:11 +0100

    Update logback-classic to 1.2.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val catsEffect        = "3.3.0"
     val fs2               = "3.2.3"
     val log4cats          = "2.1.1"
-    val logback           = "1.2.7"
+    val logback           = "1.2.8"
     val scodecBits        = "1.1.30"
     val circe             = "0.14.1"
     val scalaPb           = scalapb.compiler.Version.scalapbVersion
