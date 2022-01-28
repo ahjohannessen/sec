9edb383ef5b57dc2d4c0c5ccfc51b0684c06e212
Scala Steward <me@scala-steward.org>
2022-01-28 22:10:28 +0100

    Update cats-effect, cats-effect-testkit to 3.3.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object versions {
 
     val catsCore   = "2.7.0"
-    val catsEffect = "3.3.4"
+    val catsEffect = "3.3.5"
     val fs2        = "3.2.4"
     val log4cats   = "2.2.0"
     val logback    = "1.2.10"
