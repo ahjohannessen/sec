8076bb7a1b71c4351565f4cb728a3bc54501bb33
Scala Steward <me@scala-steward.org>
2022-04-08 10:02:29 +0200

    Update scalacheck to 1.16.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val grpc       = "1.45.1"
     val tsConfig   = "1.4.2"
 
-    val scalaCheck      = "1.15.4"
+    val scalaCheck      = "1.16.0"
     val munitDiscipline = "1.0.9"
     val munitEffect     = "1.0.7"
     val munit           = "0.7.29"
