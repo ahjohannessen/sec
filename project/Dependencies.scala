91f0808156880eb86bb0e8b3906d6ff68cce02c2
Scala Steward <scala_steward@virtuslab.com>
2024-06-06 13:10:27 +0000

    Update ip4s-core to 3.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val catsCore   = "2.12.0"
     val catsEffect = "3.5.4"
     val fs2        = "3.10.2"
-    val ip4s       = "3.5.0"
+    val ip4s       = "3.6.0"
     val log4cats   = "2.7.0"
     val logback    = "1.2.13"
     val scodecBits = "1.2.0"
