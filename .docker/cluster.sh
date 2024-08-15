d5d0eb1694487d9aea7605ddcc893c1f28736f3f
Alex Henning Johannessen <ahjohannessen@gmail.com>
2024-08-15 08:51:53 +0100

    ci: fix docker compose

--- .docker/cluster.sh
+++ .docker/cluster.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR=$(dirname $BASH_SOURCE)
 . $SCRIPT_DIR/images.env
 . $SCRIPT_DIR/shared.env
 
-docker-compose -p cluster -f $SCRIPT_DIR/gencert.yml -f $SCRIPT_DIR/cluster.ci.yml "$@"
+docker compose -p cluster -f $SCRIPT_DIR/gencert.yml -f $SCRIPT_DIR/cluster.ci.yml "$@"
--- .docker/single-node.sh
+++ .docker/single-node.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR=$(dirname $BASH_SOURCE)
 . $SCRIPT_DIR/images.env
 . $SCRIPT_DIR/shared.env
 
-docker-compose -p single -f $SCRIPT_DIR/gencert.yml -f $SCRIPT_DIR/single-node.ci.yml "$@"
+docker compose -p single -f $SCRIPT_DIR/gencert.yml -f $SCRIPT_DIR/single-node.ci.yml "$@"
