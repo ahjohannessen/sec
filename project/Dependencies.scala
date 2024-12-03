dc4944ac660b21a6a137cb5eaf5e51c3f47cf710
Scala Steward <scala_steward@virtuslab.com>
2024-12-03 14:10:27 +0000

    Update munit to 1.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
     val scalaCheck      = "1.18.1"
     val munitDiscipline = "2.0.0"
     val munitEffect     = "2.0.0"
-    val munit           = "1.0.2"
+    val munit           = "1.0.3"
 
   }
 
