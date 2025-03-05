4ef625b65e2cba2f87ce9e9c9c27759d3669cf06
Scala Steward <scala_steward@virtuslab.com>
2025-03-05 14:02:54 +0000

    Update grpc-api, grpc-core, ... to 1.71.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val unum       = "1.2.5"
     val circe      = "0.14.3"
     val scalaPb    = scalapb.compiler.Version.scalapbVersion
-    val grpc       = "1.70.0"
+    val grpc       = "1.71.0"
     val tsConfig   = "1.4.3"
 
     val scalaCheck      = "1.18.1"
